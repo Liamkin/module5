436f63b0cb59da02f0a0a501dd15d68f64fcf31c
Myroslav Liamkin <miroslav.ljamkin@gmail.com>
2015-12-29 16:37:51 +0200

    Init

---
+++ src/Array.java
@@ -0,0 +1,27 @@
+/**
+ * Created by miroslavlyamkin on 29.12.15
+ */
+public class Array{
+    public static void main(String[] args) {
+
+        int [] array = { 1, 7, 12, -2, 14, -11};
+
+        int min = array[0];
+        int max = array[0];
+
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (min > array[i]){
+                min = array[i];
+            }
+
+            if (max < array[i]){
+                max = array[i];
+            }
+        }
+
+        System.out.println("min: " + min);
+        System.out.println("max: " + max);
+    }
+}
